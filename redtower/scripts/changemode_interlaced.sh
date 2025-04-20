d4d74feb6933d4a68124898976878f435cc3a3d3
Aaron Cruces <aaroncruces@gmail.com>
2025-04-19 20:53:01 -0400

    commands

--- redtower/scripts/changemode_interlaced.sh
+++ redtower/scripts/changemode_interlaced.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 
 HORIZONTAL=${1:-640}
-VERTICAL=${2:-240}
-REFRESH=${3:-60}
+VERTICAL=${2:-480}
+REFRESH=${3:-30}
 SCREEN=${4:-VGA-1}
 SCALEHORIZONTAL=${5:-1}
 SCALEVERTICAL=${6:-2}
 
 echo "cvt..."
-CVT_RES=$(cvt $HORIZONTAL $VERTICAL $REFRESH | grep -E 'Modeline (.*)'| sed -E 's/Modeline//g')
+CVT_RES=$(cvt -i $HORIZONTAL $VERTICAL $REFRESH | grep -E 'Modeline (.*)'| sed -E 's/Modeline//g')
 MODE_NAME=$(echo $CVT_RES | grep -Eo '\".+\"')
 echo "xrandr --newmode"
 xrandr --newmode $CVT_RES 
