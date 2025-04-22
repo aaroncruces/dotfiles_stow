20d8b1735b53d4add725b2889bb2504ad478c0af
Aaron Cruces <aaroncruces@gmail.com>
2025-04-22 01:06:47 -0400

    advance

--- redtower/scripts/bcvt.sh
+++ redtower/scripts/bcvt.sh
@@ -3,15 +3,15 @@
 # parameters 1 screen. default VGA-1
 SCREEN=${1:-VGA-1}
 # parameter 1 horizontal resolution. default 640
-HORIZONTAL_RESOLUTION=${2:-640}
+HORIZONTAL_VIEWPORT=${2:-640}
 # parameter 2 vertical resolution. default 480
-VERTICAL_RESOLUTION=${3:-480}
+VERTICAL_VIEWPORT=${3:-480}
 # parameter 3  : interlaced. default no
 INTERLACED=${3:-n}
 # parameter 4: left margin. default 0
-LEFT_MARGIN=${4:-0}
+LEFT_MARGIN=${4:-30}
 # parameter 5: right margin. default 0
-RIGHT_MARGIN=${5:-0}
+RIGHT_MARGIN=${5:-10}
 # parameter 6: top margin. default 0
 TOP_MARGIN=${6:-0}
 # parameter 7: bottom margin. default 0 
@@ -23,16 +23,39 @@ SCALEVERTICAL=${9:-1}
 # parameter 10: refresh rate. default 60
 REFRESH_RATE=${10:-60}
 
-# calculate the cvt resolution
-echo "calculate the cvt resolution"
+# calculate the resolution including the margins
+HORIZONTAL_RESOLUTION=$(($HORIZONTAL_VIEWPORT + $LEFT_MARGIN + $RIGHT_MARGIN))
+VERTICAL_RESOLUTION=$(($VERTICAL_VIEWPORT + $TOP_MARGIN + $BOTTOM_MARGIN))  
+
+
+# --- calculate the cvt resolution ---
+echo "--- calculate the cvt resolution ---"
 CVT_RES=$(cvt $HORIZONTAL_RESOLUTION $VERTICAL_RESOLUTION $REFRESH_RATE | grep -E 'Modeline (.*)'| sed -E 's/Modeline//g')
 echo "CVT_RES=\$(cvt $HORIZONTAL_RESOLUTION $VERTICAL_RESOLUTION $REFRESH_RATE | grep -E 'Modeline (.*)'| sed -E 's/Modeline//g')"
 echo "CVT_RES=$CVT_RES"
-# extract the mode name
-echo "extract the mode name"
-MODE_NAME=$(echo $CVT_RES | grep -Eo '\".+\"')
-echo "MODE_NAME=\$(echo $CVT_RES | grep -Eo '\".+\"')"
-echo "MODE_NAME=$MODE_NAME"
+# --- extract the mode name ---
+echo "--- extract the mode name ---"
+
+read MODE_NAME PIXEL_CLOCK H_ACTIVE H_FRONT_PORCH_START H_SYNC_START H_TOTAL \
+     V_ACTIVE V_FRONT_PORCH_START V_SYNC_START V_TOTAL HSYNC_POLARITY VSYNC_POLARITY \
+     <<< $(echo "$CVT_RES" | awk '{print $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12}')
+
+
+# Print variables to verify
+echo "MODE_NAME: $MODE_NAME"
+echo "PIXEL_CLOCK: $PIXEL_CLOCK"
+echo "H_ACTIVE: $H_ACTIVE"
+echo "H_FRONT_PORCH_START: $H_FRONT_PORCH_START"
+echo "H_SYNC_START: $H_SYNC_START"
+echo "H_TOTAL: $H_TOTAL"
+echo "V_ACTIVE: $V_ACTIVE"
+echo "V_FRONT_PORCH_START: $V_FRONT_PORCH_START"
+echo "V_SYNC_START: $V_SYNC_START"
+echo "V_TOTAL: $V_TOTAL"
+echo "HSYNC_POLARITY: $HSYNC_POLARITY"
+echo "VSYNC_POLARITY: $VSYNC_POLARITY"
+
+exit 0
 
 # add the new mode to xrandr
 echo "xrandr --newmode $CVT_RES"
