58c96e21bd6edc792f0f7905053404eb3678bfc3
Aaron Cruces <aaroncruces@gmail.com>
2025-04-22 23:12:21 -0400

    fixes

--- stow_uninstall_common.sh
+++ stow_uninstall_common.sh
@@ -1,3 +1,2 @@
 #! /bin/bash
-stow -v --no-folding --target=$HOME --dir=$HOME/gits/dotfiles_stow/common .
 stow -vD --target=$HOME --dir=$HOME/gits/dotfiles_stow/common .  
--- stow_uninstall_redtower.sh
+++ stow_uninstall_redtower.sh
@@ -1,3 +1,2 @@
 #! /bin/bash
-stow -v --no-folding --target=$HOME --dir=$HOME/gits/dotfiles_stow/redtower .
 stow -vD --target=$HOME --dir=$HOME/gits/dotfiles_stow/redtower .  
