2165b437aff46099753fe33c6c9179366fa69903
Aaron Cruces <aaroncruces@gmail.com>
2025-06-06 01:04:07 -0400

    zsh theme

--- common/.config/zsh/modules_last/antidote_load_common.zsh
+++ common/.config/zsh/modules_last/antidote_load_common.zsh
@@ -1,3 +1,4 @@
+# loading from antidote
 source $HOME/gits/antidote/antidote.zsh
 antidote load ${ZDOTDIR:-$HOME}/.zsh_plugins.txt
 
--- common/.config/zsh/modules_last/theme_common.zsh
+++ common/.config/zsh/modules_last/theme_common.zsh
@@ -1,7 +1,9 @@
+# if in tty (including tmux)
 if [ -z "${DISPLAY}" ] && [ -n "${XDG_VTNR}" ]; then
     unset PROMPT
     PROMPT='%F{cyan}%n@%m%f %F{green}%~%f %F{white}$%f '
 else
+# if in ssh or gui (including tmux)
     unset PROMPT
     eval "$(oh-my-posh init zsh)"
 fi
