75efbbdd3f83abf4d3b8e4d6201b56239062aead
Aaron Cruces <aaroncruces@gmail.com>
2025-06-06 03:59:05 -0400

    path

--- common/.config/zsh/modules_first/path_common.zsh
+++ common/.config/zsh/modules_first/path_common.zsh
@@ -1,4 +1,6 @@
 PATH="${PATH:+${PATH}:}$HOME/.local/bin"   # appending
+PATH="${PATH:+${PATH}:}/usr/sbin"  
+
 # PATH="${PATH:+${PATH}:}$HOME/.local/texlive/2024/bin/x86_64-linux"   # appending
 # MANPATH="${MANPATH:+${MANPATH}:}$HOME/.local/texlive/2024/texmf-dist/doc/man"   # appending
 # PATH="${PATH:+${PATH}:}/opt/pspdev/bin"   # appending
