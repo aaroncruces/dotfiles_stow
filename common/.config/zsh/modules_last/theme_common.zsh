4062e0a7987eda8eb1d9fd21b632ec8105d3ef0c
Aaron Cruces <aaroncruces@gmail.com>
2025-06-06 01:05:53 -0400

    zsh theme

--- common/.config/zsh/modules_last/theme_common.zsh
+++ common/.config/zsh/modules_last/theme_common.zsh
@@ -1,4 +1,5 @@
 # if in tty (including tmux)
+# -z is "not set", -n is "set"
 if [ -z "${DISPLAY}" ] && [ -n "${XDG_VTNR}" ]; then
     unset PROMPT
     PROMPT='%F{cyan}%n@%m%f %F{green}%~%f %F{white}$%f '
