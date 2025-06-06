4d2d3fa9aebc69c924278fab5c210d7623eb0c42
Aaron Cruces <aaroncruces@gmail.com>
2025-06-06 00:56:54 -0400

    zsh theme

--- common/.config/zsh/modules_last/theme.zsh
+++ common/.config/zsh/modules_last/theme.zsh
@@ -1,5 +1,7 @@
 if [ -z "${DISPLAY}" ] && [ "${XDG_VTNR}" -eq 1 ]; then
+    unset PROMPT
     PROMPT='%F{cyan}%n@%m%f %F{green}%~%f %F{white}$%f '
 else
+    unset PROMPT
     eval "$(oh-my-posh init zsh)"
 fi
