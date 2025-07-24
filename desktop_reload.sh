81cc148842f72a724f068b86a04f28eb875e97eb
Aaron Cruces <aaroncruces@gmail.com>
2025-07-24 01:37:58 -0400

    scripts

--- desktop_reload.sh
+++ desktop_reload.sh
@@ -2,5 +2,5 @@
 bash stow_uninstall_desktop.sh
 bash stow_uninstall_common.sh
 git pull
-bash stow_install_common
-bash stow_install_desktop
+bash stow_install_common.sh
+bash stow_install_desktop.sh
